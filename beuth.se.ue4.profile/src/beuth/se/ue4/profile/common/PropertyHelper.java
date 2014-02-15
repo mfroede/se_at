eb63cffcbd720f4fe972cdebc962c9931ee98bba
Marcus Fabarius <marcus.fabarius@gmx.de>
2014-02-15 22:43:59 +0100

    button press

--- beuth.se.ue4.profile/src/beuth/se/ue4/profile/common/PropertyHelper.java
+++ beuth.se.ue4.profile/src/beuth/se/ue4/profile/common/PropertyHelper.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.impl.DynamicEObjectImpl;
 import org.eclipse.uml2.uml.Element;
@@ -13,29 +14,35 @@ import org.eclipse.uml2.uml.util.UMLSwitch;
 
 public class PropertyHelper extends UMLSwitch<String>{
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public List<Object> getTaggedValue(Element element, String stereotypeName, String property){
+	public List<Object> getTaggedValue(Element element, String stereotypeName, String property, Boolean isClass){
 		Stereotype stereotype = getStereotype(element, stereotypeName);
 		List<Object> result = new ArrayList<Object>();
 		Object values = element.getValue(stereotype, property);
-		if( values instanceof Collection<?>){
+		if( values instanceof Collection){
 			for(Object object : (Collection<List>) values) {
-				if(object instanceof DynamicEObjectImpl) {
-					final DynamicEObjectImpl objectImpl = (DynamicEObjectImpl) object;
-					final EClass c = objectImpl.eClass();
-					EStructuralFeature sf = c.getEStructuralFeature("base_Property");
-					
-					if( sf !=null ){
-						result.add(objectImpl.eDynamicGet(sf, true));
-					}
-				}
+				addToResult(property, isClass, result, object);
 			}
 		} else {
-			result.add(values);
+			addToResult(property, isClass, result, values);
 		}
-		System.out.println("==> " + result.toString());
 		return result;
-		
+	}
 
+	private void addToResult(String property, Boolean isClass,
+			List<Object> result, Object values) {
+		if(values instanceof DynamicEObjectImpl) {
+			final DynamicEObjectImpl objectImpl = (DynamicEObjectImpl) values;
+			final EClass c = objectImpl.eClass();
+			EStructuralFeature sf = null;
+			if( isClass ){
+				sf = c.getEStructuralFeature("base_Class");
+			} else {
+				sf = c.getEStructuralFeature("base_Property");
+			}
+			if( sf !=null ){
+				result.add(objectImpl.eDynamicGet(sf, true));
+			}
+		}
 	}
 	
 	private Stereotype getStereotype(Element element, String stereotypeName) {
