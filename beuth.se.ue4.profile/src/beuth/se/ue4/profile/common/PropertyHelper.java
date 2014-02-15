3e440b275e8491257be8271aa7f44eac66651ede
Marcus Fabarius <marcus.fabarius@gmx.de>
2014-02-15 19:49:37 +0100

    TaggedValue

--- beuth.se.ue4.profile/src/beuth/se/ue4/profile/common/PropertyHelper.java
+++ beuth.se.ue4.profile/src/beuth/se/ue4/profile/common/PropertyHelper.java
@@ -3,32 +3,29 @@ package beuth.se.ue4.profile.common;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.impl.DynamicEObjectImpl;
 import org.eclipse.uml2.uml.Element;
 import org.eclipse.uml2.uml.Stereotype;
+import org.eclipse.uml2.uml.util.UMLSwitch;
 
-public class PropertyHelper {
+public class PropertyHelper extends UMLSwitch<String>{
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public List<Object> getTaggedValue(Element element, String stereotypeName, String property){
 		Stereotype stereotype = getStereotype(element, stereotypeName);
-		System.out.println("=0=> " + element.toString());
-		System.out.println("=1=> " + stereotype.getQualifiedName());
 		List<Object> result = new ArrayList<Object>();
 		Object values = element.getValue(stereotype, property);
-		System.out.println("=2=> " + property);
-		if( values instanceof Collection){
-			System.out.println("=3=> " + ((Collection<List>)values).size());
+		if( values instanceof Collection<?>){
 			for(Object object : (Collection<List>) values) {
-				System.out.println("=4=> " + ((Collection<List>)values).toString());
 				if(object instanceof DynamicEObjectImpl) {
-					System.out.println("=5=> " + ((Collection<List>)values).toString());
 					final DynamicEObjectImpl objectImpl = (DynamicEObjectImpl) object;
 					final EClass c = objectImpl.eClass();
-					final EStructuralFeature sf = c.getEStructuralFeature("base_Class");
-					System.out.println("=6=> " + sf);
-					result.add(objectImpl);//.eDynamicGet(sf, true));
+					EStructuralFeature sf = c.getEStructuralFeature("base_Property");
+					
 					if( sf !=null ){
+						result.add(objectImpl.eDynamicGet(sf, true));
 					}
 				}
 			}
@@ -37,8 +34,10 @@ public class PropertyHelper {
 		}
 		System.out.println("==> " + result.toString());
 		return result;
-	}
+		
 
+	}
+	
 	private Stereotype getStereotype(Element element, String stereotypeName) {
 		for (Stereotype stereotype : element.getAppliedStereotypes()) {
 			if (stereotype.getName().equalsIgnoreCase(stereotypeName)) {
